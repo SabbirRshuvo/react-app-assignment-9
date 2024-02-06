8ae78e943fdbef7ab402f96ca81e0c9696b5e5ef
SabbirRshuvo <sabbirhshuvo11@gmail.com>
2024-02-06 23:06:22 +0600

    second commit

---
+++ src/Componets/Blogs/Blogs.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Blogs = () => {
+    return (
+        <div>
+            <h2>THis is blogs page</h2>
+        </div>
+    );
+};
+
+export default Blogs;
---
+++ src/Componets/Header/Header.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Header = () => {
+    return (
+        <div>
+            <nav>
+                <Link to='/home'>Home</Link>
+                <Link to='/reviews'>Reviews</Link>
+                <Link to='/dashboard'>Dashboard</Link>
+                <Link to='/blogs'>Blogs</Link>
+                <Link to='/about'>About</Link>
+                
+            </nav>
+            
+        </div>
+    );
+};
+
+export default Header;
